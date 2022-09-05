4bc93c22e5c2e4e5d0aaf2a9ce1354a1c83e3499
Coaixy <Coaixy@outlook.com>
2022-09-05 14:48:01 +0800

    init

---
+++ parse.py
@@ -0,0 +1,23 @@
+import json
+from operator import indexOf
+
+textData = 0
+with open('C:\\Users\\Coaixy\\Desktop\\data.json',"r",encoding="utf-8") as file:
+    textData = file.read()
+jsonData = json.loads(textData)['data']['questions']
+qc = list()
+result = dict()
+couter = 0
+for i in jsonData:
+    if not(i['title'] in qc):
+        text = ""
+        for j in i['optionList']:
+            if j['isCorrect'] == 1:
+                text = text +j['content']+"--"
+        result[i['title']] = text
+        qc.append(i['title'])
+        couter = couter+1
+print(couter)
+data = json.dumps(result,ensure_ascii=False)
+with open('C:\\Users\\Coaixy\\Desktop\\result.json',"w",encoding="utf-8") as file:
+    file.write(data)
