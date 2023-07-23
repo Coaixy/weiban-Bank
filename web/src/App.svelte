536129fbdce157b3d81a725e8f063161397d23bf
Coaixy <coaixy@outlook.com>
2023-07-23 14:32:29 +0800

    Feat:Add Top Alert

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
+    import {Alert} from '@svelteuidev/core';
+    import {InfoCircled} from 'radix-icons-svelte';
 </script>
 
 <main>
+    <Alert icon={InfoCircled} title="注意!" color="blue">
+        本网站免费开放，不会主动收取费用
+        <a href="https://jcdn.lawliet.ren/qrcode.jpg" target="_blank" class="font-bold underline">加入微信群</a>
+    </Alert>
 
 </main>
 
