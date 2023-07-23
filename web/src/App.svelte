3f57c4ad13aeffb810c5dc6b1759af6c0e48ccbc
Coaixy <coaixy@outlook.com>
2023-07-23 15:31:21 +0800

    Feat:Finish Use

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,6 +1,36 @@
 <script lang="ts">
-    import {Alert} from '@svelteuidev/core';
-    import {InfoCircled} from 'radix-icons-svelte';
+    import {Alert,Accordion } from '@svelteuidev/core';
+    import {Hand, InfoCircled} from 'radix-icons-svelte';
+    import data from "./result.json"
+    import {Input, InputWrapper} from '@svelteuidev/core';
+
+    let answerData = []
+    let question = ""
+
+
+    function debounce(func, delay) {
+        let timer = null;
+        return function (...args) {
+            if (timer) clearTimeout(timer);
+            timer = setTimeout(() => {
+                func.apply(this, args);
+            }, delay)
+        }
+    }
+
+    const submit = debounce(() => {
+        answerData = []
+        for (const key in data) {
+            if (key.includes(question)) {
+                answerData.push({
+                    'q': key,
+                    'a': data[key]
+                })
+            }
+        }
+        answerData = answerData
+    }, 1000)
+
 </script>
 
 <main>
@@ -8,7 +38,23 @@
         本网站免费开放，不会主动收取费用
         <a href="https://jcdn.lawliet.ren/qrcode.jpg" target="_blank" class="font-bold underline">加入微信群</a>
     </Alert>
-
+    <div class="mt-4">
+        <Input icon={Hand}
+               on:keyup={submit}
+               bind:value={question}
+               placeholder="请输入你的问题"
+               radius="xs"
+        />
+    </div>
+    <Accordion radius="xs">
+        {#each answerData as answer}
+            <!--{answer.q.substring(1,10)}-->
+            <Accordion.Item>
+                <div slot="control">Question : {answer.q}</div>
+                <div class="font-bold">Answer : {answer.a}</div>
+            </Accordion.Item>
+        {/each}
+    </Accordion>
 </main>
 
 <style>
